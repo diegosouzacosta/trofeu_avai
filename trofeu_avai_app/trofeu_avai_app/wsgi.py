45ffba99cb118feb3499fbb70c434bc1d894ff23
Diego Costa <diego.de.scosta@gmail.com>
2017-11-25 14:47:10 -0200

    alterado wsgi

--- trofeu_avai_app/trofeu_avai_app/wsgi.py
+++ trofeu_avai_app/trofeu_avai_app/wsgi.py
@@ -7,9 +7,9 @@ from django.core.wsgi import get_wsgi_application
 sys.path.insert(
     0, '/home/diegocosta/webapps/trofeu_avai/trofeu_avai/env/lib/python3.5/site-packages')
 sys.path.append('/home/diegocosta/webapps/trofeu_avai')
-sys.path.append('/home/diego_costa/webapps/trofeu_avai/trofeu_avai')
+sys.path.append('/home/diegocosta/webapps/trofeu_avai/trofeu_avai')
 sys.path.append(
-    '/home/diego_costa/webapps/trofeu_avai/trofeu_avai/trofeu_avai_app')
+    '/home/diegocosta/webapps/trofeu_avai/trofeu_avai/trofeu_avai_app')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trofeu_avai_app.settings")
 
